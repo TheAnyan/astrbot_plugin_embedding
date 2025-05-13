84938ce094524adc3fd52cf711e2d7c2c6712561
an_yan <1229359451@qq.com>
2025-05-13 23:03:35 +0800

    测试

--- embedding_providers.py
+++ embedding_providers.py
@@ -68,6 +68,7 @@ class BaiduProvider(Provider):
             self.access_token = await self.get_access_token()
         logger.info(f"access_token:{self.access_token}")
         async with httpx.AsyncClient(timeout=30) as client:
+            logger.info(f"baidu1")
             params = {"access_token": self.access_token}
             payload = {"input": [text]}
             headers = {"Content-Type": "application/json"}
@@ -75,6 +76,7 @@ class BaiduProvider(Provider):
                 self.url + "/" + self.model,
                 headers=headers, params=params, json=payload)
             response.raise_for_status()  # 自动处理4xx/5xx状态码
+            logger.info(f"baidu2")
             return response.json()["data"][0]["embedding"]
 
 
